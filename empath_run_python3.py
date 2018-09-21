374a9c5fec0d81c5661112849ed62cb84faa0e84
mihhus <gi33ti51@gmail.com>
2018-09-21 11:22:08 +0900

    That's all for new.

--- empath_run_python3.py
+++ empath_run_python3.py
@@ -1,9 +1,14 @@
+import re
 import subprocess
-def inputToPython2(input_text):
+def CallPython2(input_text = ""):
     p = subprocess.Popen("python empath_run_python2.py", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     stdout_data, stderr_data = p.communicate(input_text.encode("utf-8"), timeout=20)
     return stdout_data.decode("utf-8")
 
-print(inputToPython2("aaa"))
+data = CallPython2()
+print(data)
+pattern = r'([0-9]*)'
 
+lists = re.findall(pattern, data)
+print(lists)
