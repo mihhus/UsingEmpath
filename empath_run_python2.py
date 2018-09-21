e8db092b59542a8dacf7c45ef9f254d342eee74d
mihhus <gi33ti51@gmail.com>
2018-09-21 10:50:32 +0900

    API work

--- empath_run_python2.py
+++ empath_run_python2.py
@@ -2,19 +2,19 @@ from poster.encode import multipart_encode, MultipartParam
 from poster.streaminghttp import register_openers
 import urllib2
 import sys
-
+# Get data from Parent Process
 got_line = sys.stdin.readline()
 got_line = got_line.strip()
 
-url = "https://api.webempath.net/v2/analyzeWav"
+url = "https://api.webempath.net/v2/analyzeWav" #URL of Empath API
 register_openers()
 items = []
 items.append(MultipartParam('apikey', "jZNehSahdorqYqYUSPg8HYaI9fTHdl1SRvQoglcHQV8"))
-items.append(MultipartParam.from_file('wav', "recoding2.wav"))
+items.append(MultipartParam.from_file('wav', "recoding4.wav"))
 datagen, headers = multipart_encode(items)
 request = urllib2.Request(url, datagen, headers)
 response = urllib2.urlopen(request)
 if response.getcode() == 200:
-    sys.stdout.write(respone.read().encode("utf-8"))
+    sys.stdout.write(response.read())
 else:
     sys.stdout.write("HTTP status %d" % (response.getcode()))
--- empath_run_python3.py
+++ empath_run_python3.py
@@ -1,7 +1,9 @@
+import subprocess
 def inputToPython2(input_text):
-    p = subprocess.Pone("empath_run_python2.py", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PINE, stderr=subprocess.PINE)
+    p = subprocess.Popen("python empath_run_python2.py", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     stdout_data, stderr_data = p.communicate(input_text.encode("utf-8"), timeout=20)
     return stdout_data.decode("utf-8")
 
-inputToPython2("aaa")
+print(inputToPython2("aaa"))
+
